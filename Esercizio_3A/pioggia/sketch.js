5231e7e7c6922782279c5cbfa1f7e18a937d41f2
l0renz0hess <lorenzo.hess321@gmail.com>
2025-06-14 12:52:12 +0200

    esercizio pioggia aggiornato

--- Esercizio_3A/pioggia/sketch.js
+++ Esercizio_3A/pioggia/sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-	createCanvas(windowWidth, windowHeight, WEBGL)
+	createCanvas(windowWidth, windowHeight)
 }
 
 function windowResized() {
@@ -9,26 +9,15 @@ function windowResized() {
 function draw() {
 	background(0)
 
-
-	rotateX(-mouseY*0.01)
-	rotateY(-mouseX*0.01)
-
-
-
-	let dimensione = 1000
-
-	
 	for (let i=0; i<50; i++) {
 		let gl= random(50, 150)
-		let gx= random(-dimensione, dimensione)
-		let gy= random (-dimensione, dimensione - gl)
-		let gz= random(-dimensione, dimensione)
-
+		let gx= random(0, width)
+		let gy= random (-gl, height)
+	
 		strokeWeight(random(1, 3))
 		stroke(255, random(100, 255))
-		line(gx, gy, gz,      gx, gy + gl, gz)
+		line(gx, gy, gx, gy + gl)
 	}
-	
     
 
 
